65649fcc7a4ae15ddb0b67a907fa7eda7ab7c1ed
hgqh <hgqh11@gmail.com>
2023-12-27 17:58:44 +0700

    fetch home image

--- src/pages/dashboard/general/Home.js
+++ src/pages/dashboard/general/Home.js
@@ -1,68 +1,27 @@
 // @mui
 import { Container } from '@mui/material';
 // hooks
+import {useEffect, useState} from "react";
 import useSettings from "../../../hooks/useSettings";
 // components
 import Page from "../../../components/Page";
 import MasonryGallery from "../../../components/MasonryGallery";
+import {getAllPost} from "../../../api/posts";
 
 // ----------------------------------------------------------------------
 
 export default function Home() {
     const { themeStretch } = useSettings();
 
-    const items = [
-        {
-            title: 'Image Title 1',
-            author: 'Author Name 1',
-            image: 'https://picsum.photos/720/1080',
-        },
-        {
-            title: 'Image Title 2',
-            author: 'Author Name 2',
-            image: 'https://picsum.photos/1080/1920',
-        },
-        {
-            title: 'Image Title 3',
-            author: 'Author Name 3',
-            image: 'https://picsum.photos/1440/2560',
-        },
-        {
-            title: 'Image Title 4',
-            author: 'Author Name 4',
-            image: 'https://picsum.photos/1080/1920',
-        },
-        {
-            title: 'Image Title 5',
-            author: 'Author Name 5',
-            image: 'https://picsum.photos/1080/1920',
-        },{
-            title: 'Image Title 1',
-            author: 'Author Name 1',
-            image: 'https://picsum.photos/720/1080',
-        },
-        {
-            title: 'Image Title 2',
-            author: 'Author Name 2',
-            image: 'https://picsum.photos/1080/1920',
-        },
-        {
-            title: 'Image Title 3',
-            author: 'Author Name 3',
-            image: 'https://picsum.photos/1440/2560',
-        },
-        {
-            title: 'Image Title 4',
-            author: 'Author Name 4',
-            image: 'https://picsum.photos/1080/1920',
-        },
-        {
-            title: 'Image Title 5',
-            author: 'Author Name 5',
-            image: 'https://picsum.photos/1080/1920',
-        },
-        // Add more items as needed
-    ];
+    const [items, setItems] = useState([])
+    useEffect(async () => {
+        const res = await getAllPost({
+            pageSize: 10,
+            pageNumber: 1
+        })
+        setItems(res.data)
+    }, []);
+
 
     return (
         <Page title="Home">
