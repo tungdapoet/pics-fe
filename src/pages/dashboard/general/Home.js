5455a89eb93bbd7b59231188f42f57e9c7cd45c5
hgqh <hgqh11@gmail.com>
2024-01-03 21:14:32 +0700

    fix search

--- src/pages/dashboard/general/Home.js
+++ src/pages/dashboard/general/Home.js
@@ -1,7 +1,7 @@
 // @mui
 import {Button, Container, Divider, Input, InputAdornment} from '@mui/material';
 // hooks
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {styled} from "@mui/material/styles";
 import useSettings from "../../../hooks/useSettings";
 // components
@@ -16,7 +16,7 @@ import cssStyles from "../../../utils/cssStyles";
 const APPBAR_MOBILE = 64;
 const APPBAR_DESKTOP = 92;
 
-const SearchbarStyle = styled('div')(({ theme }) => ({
+const SearchbarStyle = styled('div')(({theme}) => ({
     ...cssStyles(theme).bgBlur(),
     marginBottom: 50,
     zIndex: 99,
@@ -29,11 +29,14 @@ const SearchbarStyle = styled('div')(({ theme }) => ({
 
 
 export default function Home() {
-    const { themeStretch } = useSettings();
+    const {themeStretch} = useSettings();
 
     const [search, setSearch] = useState('')
-
+    const [searchContent, setSearchContent] = useState('')
+    const [page, setPage] = useState(1)
+    const [hasNext, setHasNext] = useState(false)
     const [items, setItems] = useState([])
+    const pageEndRef = useRef(null);
 
     useEffect(async () => {
         const res = await getAllPost({
@@ -41,24 +44,71 @@ export default function Home() {
             pageNumber: 1
         })
         setItems(res.data)
+        setHasNext(res.hasNext)
     }, []);
 
-
-    const fetchPost = () => {
-        if(!search) {
-            getAllPost({
-                pageNumber: 1,
-                pageSize: 10
-            }).then((res) => {
-                setItems(res.data)
-            })
-        } else {
-            getPostByTitle({
-                title: search
-            }).then((res) => {
-                setItems(res.data)
-            })
+    useEffect( () => {
+        console.log('update hasnext')
+        if (!hasNext) {
+            return;
         }
+
+        const observerCallback = async (entries) => {
+            if (entries[0].isIntersecting && hasNext) { // If the observed element is in viewport
+                setPage((prev) => prev + 1); // Increment the page number
+            }
+        };
+
+        console.log('create')
+        const observer = new IntersectionObserver(observerCallback);
+
+        if (pageEndRef.current) {
+            console.log('observe')
+            observer.observe(pageEndRef.current);
+        }
+
+        return () => {
+            if (pageEndRef.current) {
+                console.log('unobserve')
+                observer.unobserve(pageEndRef.current);
+            }
+        };
+    }, [hasNext]);
+
+    useEffect(async () => {
+        await fetchPost()
+    }, [page, searchContent])
+
+    const fetchPost = async () => {
+        if (!searchContent) {
+            const res = await getAllPost({
+                pageNumber: page,
+                pageSize: 10
+            })
+            if(page === 1) {
+                setItems(res.data)
+            } else {
+                setItems((prevItems) => [...prevItems, ...res.data])
+            }
+            setHasNext(res.hasNext)
+        } else {
+            const res = await getPostByTitle({
+                title: searchContent,
+                pageNumber: page,
+                pageSize: 10
+            })
+            if(page === 1) {
+                setItems(res.data)
+            } else {
+                setItems((prevItems) => [...prevItems, ...res.data])
+            }
+            setHasNext(res.hasNext)
+        }
+    }
+
+    const searchPostByName = () => {
+        setSearchContent(search)
+        setPage(1)
     }
 
     return (
@@ -75,19 +125,20 @@ export default function Home() {
                         <InputAdornment position="start">
                             <Iconify
                                 icon={'eva:search-fill'}
-                                sx={{ color: 'text.disabled', width: 20, height: 20 }}
+                                sx={{color: 'text.disabled', width: 20, height: 20}}
                             />
                         </InputAdornment>
                     }
-                    sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
+                    sx={{mr: 1, fontWeight: 'fontWeightBold'}}
                 />
-                <Button variant="contained" onClick={fetchPost}>
+                <Button variant="contained" onClick={searchPostByName}>
                     Search
                 </Button>
             </SearchbarStyle>
             <Container maxWidth={themeStretch ? false : 'xl'}>
-                <MasonryGallery items={items} />
+                <MasonryGallery items={items}/>
             </Container>
+            {hasNext ? <div ref={pageEndRef}>Load more</div> : <div>End of content</div>}
         </Page>
     );
 }
